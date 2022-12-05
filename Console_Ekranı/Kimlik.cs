a82b862e726375d5b2523ea59283a4d41e64ac4f
Cüneyt <67542285+cuneytsener53@users.noreply.github.com>
2022-12-05 15:13:58 +0300

    Add files via upload

---
+++ Console_Ekranı/Kimlik.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Ekranı
+{
+    internal class Kimlik
+    {
+
+        string ad;
+        string soyad;
+        string memleket;
+        int yas;
+        char cinsiyet;
+
+        public  string AD
+        {
+            get { return ad; }
+            set { ad = value.ToLower(); }
+        }
+
+        public string SOYAD
+        {
+            get { return soyad; } 
+            set { soyad = value.ToLower(); }
+        }
+        public string MEMLEKET
+        {
+            get { return memleket; }
+            set { memleket = value.ToLower(); }
+        }
+
+        public int YAS
+        {
+            get { return yas; }
+            set { yas = Math.Abs(value); }
+        }
+
+        public char CİNSİYET
+        {
+
+            get { return cinsiyet; }
+            set { cinsiyet = value; }
+        }
+
+        public Kimlik()
+        {
+            ad = "";
+            soyad = "";
+            yas = 18;
+            cinsiyet = 'K';
+            memleket = "Rize";
+
+
+        }
+    }
+}
---
+++ Console_Ekranı/Program.cs
@@ -0,0 +1,24 @@
+﻿using Console_Ekranı;
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Kimlik kml = new Kimlik();
+            kml.AD = "Cüneyt";
+            kml.SOYAD = "Şener";
+            kml.MEMLEKET = "Rize";
+            kml.CİNSİYET = 'E';
+            kml.YAS = 28;
+            Console.WriteLine(kml.AD);
+            Console.WriteLine(kml.SOYAD);
+            Console.WriteLine(kml.MEMLEKET);
+            Console.WriteLine(kml.CİNSİYET);
+            Console.WriteLine(kml.YAS);
+            Console.Read();
+        }
+    }
+}
